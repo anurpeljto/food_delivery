47e72b541222df0fe6963416e2f58d7fa9b81a08
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-04-03 19:51:02 +0200

    Create basic restaurant screen components

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -20,6 +20,29 @@ export default function RestaurantScreen() {
             <Icon.ArrowLeft strokeWidth="3" stroke={ThemeColors.bgColor(1)} />
           </TouchableOpacity>
         </View>
+        <View style={{borderTopLeftRadius: 40, borderTopRightRadius: 40}} className="bg-white -mt-12 pt-6 ">
+          <View className="px-5">
+            <Text className="text-3xl font-bold">{item.name}</Text>
+            <View className="flex-row font-semibold space-x-2 my-2">
+              <View className="flex-row items-center space-x-1">
+                      <Icon.Star className="h-4 w-4" fill="yellow" stroke="orange"></Icon.Star>
+                      <Text className="text-xs">
+                          <Text className={item.stars > 3 ? ' text-green-700 ' : 'text-red-700'}>{item.stars} </Text>
+                          <Text className ="text-gray-500">
+                              ({item.reviews} reviews) | <Text className="font-semibold">{item.category}</Text>
+                          </Text>
+                      </Text>
+                  </View>
+                  <View className="flex-row items-center space-x-1">
+                      <Icon.MapPin color="red" width="15" height="15"></Icon.MapPin>
+                      <Text className="text-gray-700 text-xs ">{item.address}</Text>
+                  </View>
+              </View>
+
+              {/* add descritpion */}
+              <Text className="text-gray-500 mt-2">{item.description}</Text>
+          </View>
+        </View>
       </ScrollView>
     </View>
   )
