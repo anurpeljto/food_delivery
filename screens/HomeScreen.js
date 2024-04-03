414725d88658693c0c1b0364361b4443e380b0fe
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-04-03 11:34:50 +0200

    Create searchbar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,18 +4,28 @@ import React from 'react'
 import { StyleSheet } from 'react-native'
 import * as Icon from 'react-native-feather';
 import { StatusBar } from 'expo-status-bar';
+import { ThemeColors } from '../theme';
 
   
   export default function HomeScreen() {
     return (
       <SafeAreaView className="bg-white">
         <StatusBar barStyle="dark-content" />
-            <View className="flex-row items-center space-x-2 px-4 pb-2">
+            <View className="flex-row items-center space-x-2 px-4 pb-4">
                 <View className="flex-row flex-1 items-center p-3 rounded-full border border-gray-300">
                     <Icon.Search height="25" width="25" stroke="gray"></Icon.Search>
                     <TextInput placeholder='Restaurants' className="ml-2 flex-1" />
+                    <View className="flex-row items-center space-x-1 border-0 border-l-2 pl-2 border-l-gray-300">
+                      <Icon.MapPin height="20" width="20" stroke="gray" />
+                      <Text className="text-gray-600">Sarajevo, FBiH</Text>
+                    </View>
+                </View>
+                <View className="p-3 rounded-full" style={{backgroundColor: ThemeColors.bgColor(1)}}>
+                  <Icon.Sliders height="20" width="20" strokeWidth={2.5} stroke="white"></Icon.Sliders>
                 </View>
             </View>
+
+            
       </SafeAreaView>
     )
   }
